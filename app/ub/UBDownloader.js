6736c1f4933221d7eb2b006c0e3a83a8be886ee0
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-05-27 23:36:15 +0800

    Sat May 27 11:36:15 PM CST 2023

--- app/ub/UBDownloader.js
+++ app/ub/UBDownloader.js
@@ -1,6 +1,6 @@
-const PodcastFeedBuilder = require('./podcast/PodcastFeedBuilder.js')
+const PodcastFeedBuilder = require('./../podcast/PodcastFeedBuilder.js')
 
-const UBInfo = require('./ub/UBInfo.js')
+const UBInfo = require('./UBInfo.js')
 
 let Parser = require('rss-parser');
 let parser = new Parser();
@@ -10,11 +10,11 @@ const fs = require('fs');
 module.exports = async function (feedURL, filter) {
   // 先取得
   if (fs.existsSync('/output/d.txt')) {
-    console.log(prev, fs.readFileSync('/output/d.txt', 'utf8'))
+    console.log('[prev]', fs.readFileSync('/output/d.txt', 'utf8'))
   }
   
 
-  let next = new Date().toUTCString()
-  console.log(next, next)
+  let next = (new Date()).toUTCString()
+  console.log('[next]', next)
   fs.writeFileSync('/output/d.txt', next, 'utf8')
 }
