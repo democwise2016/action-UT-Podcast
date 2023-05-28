8cba6f4c583597f3feaa496c9632586f9ad7ac77
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-05-28 22:29:56 +0800

    Sun May 28 10:29:56 PM CST 2023

--- app/index.js
+++ app/index.js
@@ -1,6 +1,11 @@
 const UBDownloader = require('./ub/UBDownloader.js')
 const FeedIndexBuilder = require('./podcast/FeedIndexBuilder.js')
 const CONFIG = require('./../config.js')
+const fs = require('fs')
+
+function randomResortArray(arr) {
+  return arr.sort(() => Math.random() - 0.5);
+}
 
 let main = async () => {
 
@@ -13,6 +18,22 @@ let main = async () => {
   // let info = await UBInfo.load(`https://youtu.be/I2D9M2QmdrA`)
   // console.log(info)
 
+  // 先檢查看看有沒有人還沒建立
+  let newArrial = false
+  for (let i = 0; i < feedList.length; i++) {
+    let feedItem = feedList[i]
+
+    if (fs.existsSync('/output/' + feedItem.feedFilename + '.rss') === false) {
+      newArrial = true
+      console.log('Start new arrial mode')
+      break
+    }
+  }
+
+  feedList.sort(() => Math.random() - 0.5)
+
+  let newArrialCount = 0
+
   // https://www.youtube.com/@LINETODAYWORLD
   for (let i = 0; i < feedList.length; i++) {
     let feedItem = feedList[i]
@@ -23,7 +44,17 @@ let main = async () => {
     // let {url, itemFilter, options} = feedList[i]
     console.log(`[${i}/${feedList.length}]`, 'Checking ', feedList[i].title, feedList[i].feedFilename, new Date().toISOString())
     try {
-      await UBDownloader(feedList[i])
+      if (newArrial === false) {
+        await UBDownloader(feedList[i])
+      }
+      if (fs.existsSync('/output/' + feedItem.feedFilename + '.rss') === false) {
+        UBDownloader(feedList[i])
+        newArrialCount++
+        if (newArrialCount >= 3) {
+          console.log('Exit new arrial mode')
+          break
+        }
+      }
     }
     catch (e) {
       console.error(e)
