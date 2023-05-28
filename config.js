30ebe07cd322b1e13511d0b1b8fea505d446d65a
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-05-29 02:39:55 +0800

    Mon May 29 02:39:55 AM CST 2023

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const ConfigCheck = require('./app/config/ConfigCheck.js')
 let CONFIG = {
   /**
    * 結尾要有/
-   * 20230529-0112 aaa a a
+   * 20230529-0112 aaa a a ad
    */
   publicURL: `https://democwise2016.github.io/action-UT-Podcast/`,
   publicURLShorten: `https://ppt.cc/fMYRpx`,
