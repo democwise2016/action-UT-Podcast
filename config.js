be8170286109a39c5eb458d98e66cd6f957ee2fb
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-08-04 20:27:01 +0800

    西元2023年08月04日 (週五) 20時27分01秒 CST

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ let CONFIG = {
   maxDownloadFeed: 10,
   newArrialMax: 5,
   maxDownloadItems: 5,
-  maxExcutionMinutes: 15,
+  maxExcutionMinutes: 13,
   feedList: [
     {
       title: 'TODAY 看世界',
