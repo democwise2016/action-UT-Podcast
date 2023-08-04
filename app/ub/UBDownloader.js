622d148e39e1b3c4ee1b7bbac9e7a5b190fbc025
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-08-05 01:33:50 +0800

    西元2023年08月05日 (週六) 01時33分50秒 CST

--- app/ub/UBDownloader.js
+++ app/ub/UBDownloader.js
@@ -68,6 +68,6 @@ module.exports = async function (feedItem = {}) {
   RSSBuildCounter++
   console.log([`[UBDownloader] RSS build`, feedFilename, RSSBuildCounter, (new Date().toISOString())].join('\t'))
 
-  await ShellSpawn([`chmod`, `-R`, `777`, `/output/*`])
+  // await ShellSpawn([`chmod`, `-R`, `777`, `/output/*`])
   // console.log([`[UBDownloader] Chmod finish`, feedFilename, RSSBuildCounter, (new Date().toISOString())].join('\t'))
 }
