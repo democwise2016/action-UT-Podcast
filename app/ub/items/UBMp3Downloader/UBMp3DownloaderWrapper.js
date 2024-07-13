613a514548c73297ec80d4579bba5f21260ec074
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-07-13 22:38:52 +0800

    西元2024年07月13日 (週六) 22時38分52秒 CST

--- app/ub/items/UBMp3Downloader/UBMp3DownloaderWrapper.js
+++ app/ub/items/UBMp3Downloader/UBMp3DownloaderWrapper.js
@@ -100,8 +100,9 @@ module.exports = async function (videoID, output, options = {}) {
     try {
       let outputPython = `"${output}"`
       let commands = ["python3", "/app/python/ub.py", url, outputPython]
-      console.log(commands)
+      // console.log(commands)
       await ShellSpawn(commands)
+      await ShellSpawn(['ls', '-l', output.slice(0, output.lastIndexOf('/'))])
       showDownloadEndMessage()
     }
     catch (e) {
