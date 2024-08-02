f8d015cf1b844e45ef4c2b2cdc091eb3e5871ed4
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-08-02 20:36:47 +0800

    西元2024年08月02日 (週五) 20時36分47秒 CST

--- config-list.js
+++ config-list.js
@@ -2,60 +2,6 @@ const ItemFiltersPreset = require('./app/config/ItemFiltersPreset.js')
 
 let feedList = [
   {
-    title: 'TODAY 看世界',
-    feedID: 'today-everyday',
-    homepageURL: 'https://www.youtube.com/channel/UCmMnzrvnsSnv-0u9M1Rxiqw',
-    feedURL: 'https://www.youtube.com/feeds/videos.xml?channel_id=UCmMnzrvnsSnv-0u9M1Rxiqw',
-    itemFilters: [
-      ItemFiltersPreset.between3minTo30Min
-    ],
-    // options: {
-    //   maxItems: 3
-    // }
-  },
-  {
-    title: '阿哲[哲週來尬電]',
-    feedID: 'linzin-news2',
-    thumbnailBorderColor: true,
-    homepageURL: 'https://www.youtube.com/channel/UC0oosHZ4k1o-zNT21gg5O7A',
-    feedURL: 'https://www.youtube.com/feeds/videos.xml?channel_id=UC0oosHZ4k1o-zNT21gg5O7A',
-    itemFilters: [
-      ItemFiltersPreset.between3minTo30Min,
-      (item) => { return (item.title.indexOf('[哲週來尬電') > -1) }
-    ],
-    options: {
-      maxItems: 3
-    }
-  },
-
-  {
-    title: '老鵝特搜',
-    feedID: 'omgoosetw-news',
-    thumbnailBorderColor: true,
-    homepageURL: 'https://www.youtube.com/channel/UC_iwP8I13JnL54BypRCzldw',
-    itemFilters: [
-      ItemFiltersPreset.between3minTo30Min,
-      (item) => { return (item.title.indexOf('｜老鵝特搜') > -1) }
-    ],
-    options: {
-      maxItems: 3
-    }
-  },
-  {
-    title: '斐姨所思【阿姨想知道】',
-    feedID: 'fanamericantime-interview',
-    thumbnailBorderColor: true,
-    homepageURL: 'https://www.youtube.com/channel/UC2VKL-DkRvXtWkfjMzkYvmw',
-    feedURL: 'https://www.youtube.com/feeds/videos.xml?channel_id=UC2VKL-DkRvXtWkfjMzkYvmw',
-    itemFilters: [
-      ItemFiltersPreset.between10minTo60Min,
-      (item) => { return (item.title.indexOf('斐姨所思【阿姨想知道】') > -1) },
-    ],
-    options: {
-      maxItems: 3
-    }
-  },
-  {
     title: '斐姨所思《行動代號2027》',
     feedID: 'fanamericantime-code2027',
     thumbnailBorderColor: true,
@@ -67,49 +13,10 @@ let feedList = [
     ],
   },
   {
-    title: '淇食很科學',
-    feedID: 'HiThisIsAchi',
-    homepageURL: 'https://www.youtube.com/channel/UCyYGbBvicdjDvNEehOMEy4A',
-    itemFilters: ItemFiltersPreset.between3minTo30Min,
-  },
-  {
-    title: '叉雞說故事',
-    feedID: 'bbqporkchicken',
-    homepageURL: 'https://www.youtube.com/channel/UCB3pBfnruGVgbP1r5Ya2CEg',
-    itemFilters: ItemFiltersPreset.between3minTo30Min,
-  },
-  {
-    title: '異色檔案說故事',
-    feedID: 'mystery2018',
-    homepageURL: 'https://www.youtube.com/channel/UCVwlKAna1gU30r3s9je06lA',
-    itemFilters: ItemFiltersPreset.between3minTo30Min,
-  },
-  {
-    title: '攝徒日記Fun TV',
-    feedID: 'funtv8964',
-    homepageURL: 'https://www.youtube.com/channel/UCvTe3Z7TZsjGzUERx4Ce6zA',
-    itemFilters: ItemFiltersPreset.between3minTo30Min,
-  },
-  {
     title: '動畫小坑谷',
     feedID: 'valleylife351',
     homepageURL: 'https://www.youtube.com/channel/UCbCb-ZUoKwQ8vcRbrH2nE0Q',
     itemFilters: ItemFiltersPreset.between1minTo10Min,
-  },
-  {
-    title: '井川一聊動畫',
-    feedID: 'InokawaHajime',
-    homepageURL: 'https://www.youtube.com/channel/UCcHVKeT_5Ta-gTa-sgooQxQ',
-    itemFilters: ItemFiltersPreset.between3minTo30Min,
-  },
-  {
-    title: '公視中晝新聞',
-    feedID: 'PNNPTS-noon-news',
-    homepageURL: 'https://www.youtube.com/playlist?list=PLjjrV9IhkIpcIqZiUfkkxgKtoiD517Kdf',
-    itemFilters: ItemFiltersPreset.between10minTo60Min,
-    options: {
-      maxItems: 1
-    }
   },
   {
     title: '鐵道事務所',
@@ -124,42 +31,10 @@ let feedList = [
     itemFilters: ItemFiltersPreset.between3minTo30Min,
   },
   {
-    title: '蒼藍鴿聊醫學',
-    feedID: 'bluepigeon0810',
-    homepageURL: 'https://www.youtube.com/channel/UCUn77_F5A65HViL9OEvIpLw',
-    itemFilters: ItemFiltersPreset.between3minTo30Min,
-  },
-  {
     title: '敖厂长',
     feedID: 'hawkaoaoful',
     homepageURL: 'https://www.youtube.com/channel/UCCkMW93Am1pLfk2nZFKAmbQ',
     itemFilters: ItemFiltersPreset.between3minTo30Min,
-  },
-  {
-    title: '萊斯 新Game報',
-    feedID: 'LiceMoo-GameNews',
-    homepageURL: 'https://www.youtube.com/channel/UC9WiXJEyHMGRqL-__3FIBEw',
-    thumbnailBorderColor: true,
-    itemFilters: [
-      ItemFiltersPreset.between3minTo30Min,
-      (item) => { return (item.title.indexOf('《新Game報') > -1) }
-    ],
-    options: {
-      maxItems: 3
-    }
-  },
-  {
-    title: '大閒者 偷閒加油站',
-    feedID: 'Idlers-GameNews',
-    homepageURL: 'https://www.youtube.com/channel/UCU6nhA37pbvzw-JXhAB87Mg',
-    thumbnailBorderColor: true,
-    itemFilters: [
-      ItemFiltersPreset.between3minTo30Min,
-      (item) => { return (item.title.indexOf('偷閒加油站') > -1) }
-    ],
-    options: {
-      maxItems: 3
-    }
   },
   {
     title: 'IC实验室聊商業',
@@ -192,34 +67,9 @@ let feedList = [
     ],
   },
   {
-    title: 'SHIN LI信用卡優惠',
-    feedID: 'SHINLI',
-    homepageURL: 'https://www.youtube.com/channel/UCK-qc_POQZwWrMg-Pr-oYtg',
-    itemFilters: ItemFiltersPreset.between3minTo30Min,
-    //maxItems: 2, // default 10
-    options: {
-      maxItems: 3
-    }
-  },
-  {
     title: '人妻聊日本企業',
     feedID: 'tacchan',
     homepageURL: 'https://www.youtube.com/channel/UCge049AFxJnXWPxYzGKoWyQ',
-    itemFilters: ItemFiltersPreset.between3minTo30Min,
-  },
-  {
-    title: '志祺七七 X 圖文不符',
-    feedID: 'shasha77',
-    homepageURL: 'https://www.youtube.com/channel/UCiWXd0nmBjlKROwzMyPV-Nw',
-    itemFilters: [
-      ItemFiltersPreset.between3minTo30Min,
-      (item) => { return (item.title.endsWith('｜志祺七七')) }
-    ] // 霸道總裁攀岩開會、網紅天天搞小團體...《絕世網紅》劇情超浮誇？《 志祺今天不讀稿 》EP010｜志祺七七
-  },
-  {
-    title: '肥宅MS',
-    feedID: 'otakumsvideo',
-    homepageURL: 'https://www.youtube.com/channel/UCBOBS5RdHd2Owytoj4PqPqA',
     itemFilters: ItemFiltersPreset.between3minTo30Min,
   },
   {
@@ -232,18 +82,6 @@ let feedList = [
     title: '四處觀察',
     feedID: 'sichuguancha',
     homepageURL: 'https://www.youtube.com/channel/UC6OeJCR9gHsJPVyNhXfK4tA',
-    itemFilters: ItemFiltersPreset.between3minTo30Min,
-  },
-  {
-    title: '好味營養師品瑄',
-    feedID: 'dietitian_pink',
-    homepageURL: 'https://www.youtube.com/channel/UCLwFOT4tHGaK9kqXXExhPFQ',
-    itemFilters: ItemFiltersPreset.between3minTo30Min,
-  },
-  {
-    title: '瑩真律師',
-    feedID: 'LawyerAngela',
-    homepageURL: 'https://www.youtube.com/channel/UCLzWMcpNlhHo0c0yOyWksvQ',
     itemFilters: ItemFiltersPreset.between3minTo30Min,
   },
   {
@@ -270,15 +108,6 @@ let feedList = [
       (item) => { return (item.title.indexOf('〖遊戲不止〗') > -1) }
     ]
   },
-  {
-    title: '關鍵評論網：國際話題',
-    feedID: 'TheNewsLens',
-    homepageURL: 'https://www.youtube.com/channel/UC4bokYuSrVGpI6_WYv7Gdbw',
-    itemFilters: [
-      ItemFiltersPreset.between3minTo30Min,
-      (item) => { return ((item.title.indexOf('｜國際大風吹') > -1) || (item.title.indexOf('｜國際值日生') > -1)) }
-    ]
-  },
   // {
   //   title: '迷走大學 時事評論',
   //   feedID: 'meisouniv-news',
@@ -292,24 +121,6 @@ let feedList = [
   //   }
   // },
   {
-    title: '迷走大學 迷大Jo報',
-    feedID: 'meisouniv-weekly-news',
-    homepageURL: 'https://www.youtube.com/channel/UCiCOKR_WkqZuYN9fpjWqnzg',
-    itemFilters: [
-      ItemFiltersPreset.between30minTo180Min,
-      (item) => { return (item.title.indexOf('【迷大Jo報】') > -1 || item.title.indexOf('【時事閒聊】') > -1 || item.title.indexOf('【走漏消息') > -1) }
-    ],
-    options: {
-      maxItems: 3
-    }
-  },
-  {
-    title: '我的學習筆記',
-    feedID: 'mynotebooks',
-    homepageURL: 'https://www.youtube.com/channel/UCAS8QqEyGGH71xYgFzNSbuw',
-    itemFilters: ItemFiltersPreset.between3minTo30Min,
-  },
-  {
     title: '孫沁岳 漫遊快譯通',
     feedID: 'yorke',
     homepageURL: 'https://www.youtube.com/channel/UCD114v8IMP5HWSUXrIJlJuQ',
@@ -317,12 +128,6 @@ let feedList = [
       ItemFiltersPreset.between6minTo60Min,
       (item) => { return ((item.title.indexOf('【漫遊快譯通】') > -1)) }
     ]
-  },
-  {
-    title: '卡提諾狂新聞',
-    feedID: 'CrazyNews9487',
-    homepageURL: 'https://www.youtube.com/playlist?list=PLH2b4YnNI7j13eD1KdpEV3t7VEWtRQlYY',
-    itemFilters: ItemFiltersPreset.between3minTo30Min,
   },
   {
     title: 'Gamker攻壳官方频道',
@@ -334,27 +139,12 @@ let feedList = [
     ]
   },
   {
-    title: '公視P#新聞實驗室',
-    feedID: 'Ppsharp_newslab',
-    homepageURL: 'https://www.youtube.com/channel/UCMDcOT4z7GS1SRGG2g7z43g',
-    itemFilters: ItemFiltersPreset.between3minTo30Min,
-  },
-  {
     title: '漫天大坑-Kay',
     feedID: 'kayshi_0521',
     homepageURL: 'https://www.youtube.com/channel/UC29L37o2cslmXRhOLn04bCA',
     itemFilters: [
       ItemFiltersPreset.between3minTo30Min,
       (item) => { return ((item.title.indexOf('動畫鑑賞集錦') === -1) || (item.title.indexOf('作畫回顧') === -1)) }
-    ]
-  },
-  {
-    title: '玩家機密',
-    feedID: 'GamerSecret',
-    homepageURL: 'https://www.youtube.com/channel/UCmtmIF-u0ojej0y9i9STuIw',
-    itemFilters: [
-      ItemFiltersPreset.between3minTo30Min,
-      (item) => { return ((item.title.indexOf('【每週新聞】') === -1)) }
     ]
   },
   {
@@ -377,18 +167,6 @@ let feedList = [
     itemFilters: ItemFiltersPreset.between3minTo30Min,
   },
   {
-    title: 'cheap講歷史',
-    feedID: 'cheapaoe',
-    homepageURL: 'https://www.youtube.com/channel/UCGGrblndNzi86WY5lJkQJiA',
-    itemFilters: ItemFiltersPreset.between3minTo30Min,
-  },
-  {
-    title: '窮奢極欲',
-    feedID: 'withmoney',
-    homepageURL: 'https://www.youtube.com/channel/UCA0o60mhG0v2Eha8wSL3_Jw',
-    itemFilters: ItemFiltersPreset.between3minTo30Min,
-  },
-  {
     title: 'Leonard',
     feedID: 'leonard2834',
     homepageURL: 'https://www.youtube.com/channel/UC1mx_wcSHtfpLk5N_zY0TRg',
@@ -407,34 +185,6 @@ let feedList = [
   //   }
   // },
   {
-    title: '波特王好帥 粉紅特報',
-    feedID: 'Realpotterking-Pinker',
-    homepageURL: 'https://www.youtube.com/channel/UCsBP1dmKYfcorJ17kfOUTvg',
-    // thumbnailBorderColor: true,
-    itemFilters: [
-      ItemFiltersPreset.between3minTo30Min,
-      (item) => { return ((item.title.indexOf('粉紅特報') >-1)) }
-    ],
-  },
-  {
-    title: '小鄭在日本',
-    feedID: 'realxiaozheng',
-    homepageURL: 'https://www.youtube.com/channel/UCn2WdaugAANMkiXfUcCNdtA',
-    // thumbnailBorderColor: true,
-    itemFilters: [
-      ItemFiltersPreset.between3minTo30Min
-    ],
-  },
-  {
-    title: '阿先說故事',
-    feedID: 'AhFirst666',
-    homepageURL: 'https://www.youtube.com/channel/UCz0V_SLhfJYhRQbVtyjNAOw',
-    // thumbnailBorderColor: true,
-    itemFilters: [
-      ItemFiltersPreset.between3minTo30Min
-    ],
-  },
-  {
     title: '社長Kuma',
     feedID: 'Super_Kuma',
     homepageURL: 'https://www.youtube.com/channel/UC4_ofgezp9nrPXHSmZMT97g',
@@ -444,73 +194,9 @@ let feedList = [
     ],
   },
   {
-    title: 'PanSci 泛科學',
-    feedID: 'PanScitw',
-    homepageURL: 'https://www.youtube.com/channel/UCuHHKbwC0TWjeqxbqdO-N_g',
-    itemFilters: ItemFiltersPreset.between3minTo30Min,
-  },
-  {
-    title: '我是RT',
-    feedID: 'RTisme',
-    homepageURL: 'https://www.youtube.com/channel/UCcG2HsNjnAbWOT6E1DRxjGA',
-    itemFilters: ItemFiltersPreset.between3minTo30Min,
-  },
-  {
-    title: 'Kuma桑',
-    feedID: 'kumasan0720',
-    homepageURL: 'https://www.youtube.com/channel/UC7S_3Gc5_vIpeFmgPG8DhNQ',
-    itemFilters: ItemFiltersPreset.between3minTo30Min,
-  },
-  {
-    title: '狐狸牧場 foxranch',
-    feedID: 'foxranch',
-    homepageURL: 'https://www.youtube.com/channel/UCV4mpMSInbFxAIAGFk8e_JQ',
-    // thumbnailBorderColor: true,
-    itemFilters: [
-      ItemFiltersPreset.between3minTo30Min,
-    ],
-  },
-  {
     title: '文字與資本主義 冏冏',
     feedID: 'Kyontw828',
     homepageURL: 'https://www.youtube.com/channel/UC_xVdv15MR17rQf-nUl92MA',
-    itemFilters: ItemFiltersPreset.between3minTo30Min,
-  },
-  {
-    title: '35線上賞屋',
-    feedID: '35visitchannel',
-    homepageURL: 'https://www.youtube.com/channel/UCnWB4yjKnm6AeW-pj4E3dQw',
-    itemFilters: ItemFiltersPreset.between3minTo30Min,
-  },
-  {
-    title: '關我什麼事 財經政論',
-    feedID: 'GUNaSHOW',
-    homepageURL: 'https://www.youtube.com/channel/UCMpuxj_U02VaHfaPD5Jjx_Q',
-    itemFilters: ItemFiltersPreset.between30minTo180Min,
-    options: {
-      maxItems: 1
-    }
-  },
-  {
-    title: 'A Jie手遊快篩',
-    feedID: 'AJieLive-mobile',
-    homepageURL: 'https://www.youtube.com/channel/UCYKhjw4BT72a1DYDzbXeb_g',
-    // thumbnailBorderColor: true,
-    itemFilters: [
-      ItemFiltersPreset.between3minTo30Min,
-      (item) => { return ((item.title.indexOf('【手遊快篩】') >-1)) }
-    ],
-  },
-  {
-    title: 'Dio Chan聊遊戲',
-    feedID: 'diochan1055',
-    homepageURL: 'https://www.youtube.com/channel/UCAQmUESq4GxO1b_iBCrYlTA',
-    itemFilters: ItemFiltersPreset.between3minTo30Min,
-  },
-  {
-    title: '嚼豆 聊遊戲王',
-    feedID: 'DOW2233',
-    homepageURL: 'https://www.youtube.com/channel/UCerJk0-d22M7MFy8opOuyjA',
     itemFilters: ItemFiltersPreset.between3minTo30Min,
   },
   {
@@ -547,12 +233,6 @@ let feedList = [
     title: 'KAZBOM 聊音樂',
     feedID: 'kazbom',
     homepageURL: 'https://www.youtube.com/channel/UCOBVyJhiDg39fjjXPWbyOqg',
-    itemFilters: ItemFiltersPreset.between3minTo30Min,
-  },
-  {
-    title: '公子沈 評中國',
-    feedID: 'gongzishen',
-    homepageURL: 'https://www.youtube.com/channel/UCrGSFNEBmCN0rqhATZels2Q',
     itemFilters: ItemFiltersPreset.between3minTo30Min,
   },
   {
