2db82668522f22551062e6aa0e1f2b21267dd90e
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-08-03 16:35:33 +0800

    西元2024年08月03日 (週六) 16時35分33秒 CST

---
+++ config-json.js
@@ -0,0 +1,20 @@
+let CONFIG = {
+  publicURL: `https://democwise2016.github.io/action-UT-Podcast/`,
+  publicURLShorten: `https://ppt.cc/fMYRpx`,
+  thumbnailBorderColor: '2980b9',
+
+  titleAppend: '',
+  titleSuffix: ' [UT]',
+  idAppend: false,
+
+  maxDownloadItemPerFeed: 50,
+  maxDownloadFeed: 50,
+  newArrialMax: 50,
+  maxDownloadItems: 100,
+  maxExcutionMinutes: 25,
+
+  maxFileCache: 5000,
+  // feedList
+}
+
+module.exports = CONFIG
